725551eee2fc7a6d7119e2b2950373cfad12a997
Amin PASBAN <aminpasban@live.com>
2023-04-22 23:45:03 +0330

    sace

--- main.go
+++ main.go
@@ -1,39 +1,40 @@
 package main
-import (   
+
+import (
 	"bufio"
 	"fmt"
+	"net/url"
 	"os"
-    "net/url"
-	"golang.org/x/exp/slices"
-	)
 
-	
+	"golang.org/x/exp/slices"
+)
+
 func main() {
 	var urls []string
 	var params []string
-	
+
 	sa := bufio.NewScanner(os.Stdin)
 	for sa.Scan() {
-			urls = append(urls,sa.Text())
+		urls = append(urls, sa.Text())
 	}
-	
+
 	if sa.Err() != nil {
 		// Handle error.
 	}
 	for _, val := range urls {
 		u, err := url.Parse(val)
 		if err != nil {
-			panic(err)
+			continue
 		}
 		if u.RawQuery != "" {
 			m, _ := url.ParseQuery(u.RawQuery)
 			for k := range m {
 				if slices.Contains(params, k) == false {
-					params = append(params,k)
+					params = append(params, k)
 					fmt.Println(k)
 				}
 			}
 		}
-		
+
 	}
 }
